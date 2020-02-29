01d26f14855d0ce1a4381567c31c12d0075412a6
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2020-03-01 00:51:40 +0700

    Update

--- models/DeepSpeech2.py
+++ models/DeepSpeech2.py
@@ -12,7 +12,7 @@ class DeepSpeech2:
 
     def __call__(self, features):
         layer = features
-        for i in range(2):
+        for i in range(3):
             layer = tf.keras.layers.Conv2D(filters=32, kernel_size=(21, 11),
                                            strides=(1, 2), padding="same")(layer)
             layer = tf.keras.layers.BatchNormalization()(layer)
@@ -25,7 +25,7 @@ class DeepSpeech2:
         layer = tf.reshape(layer, [batch_size, -1, feat_size * channel])
 
         # RNN layers
-        for i in range(2):
+        for i in range(3):
             layer = tf.keras.layers.Bidirectional(
                 tf.keras.layers.LSTM(128, return_sequences=True,
                                      recurrent_dropout=0.2))(layer)
@@ -42,7 +42,7 @@ class DeepSpeech2RowConv:
 
     def __call__(self, features):
         layer = features
-        for i in range(2):
+        for i in range(3):
             layer = tf.keras.layers.Conv2D(filters=32, kernel_size=(21, 11),
                                            strides=(1, 2), padding="same")(layer)
             layer = tf.keras.layers.BatchNormalization()(layer)
@@ -56,7 +56,8 @@ class DeepSpeech2RowConv:
 
         # RNN layers
         for i in range(3):
-            layer = tf.keras.layers.LSTM(self.rnn_unit, return_sequences=True)(layer)
+            layer = tf.keras.layers.LSTM(self.rnn_unit, return_sequences=True,
+                                         recurrent_dropout=0.2)(layer)
             layer = RowConv1D(filters=self.rnn_unit, future_context=2, strides=1,
                               padding="same")(layer)
             layer = tf.keras.layers.BatchNormalization()(layer)
