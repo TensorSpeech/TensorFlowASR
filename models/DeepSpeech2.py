dea39fe32bde4b37801e74d79e91e53bd74f5719
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2020-03-01 17:36:01 +0700

    Change ctc loss

--- models/DeepSpeech2.py
+++ models/DeepSpeech2.py
@@ -1,3 +1,7 @@
+"""
+Read https://www.tensorflow.org/api_docs/python/tf/keras/layers/LSTM
+to use cuDNN-LSTM
+"""
 from __future__ import absolute_import
 
 import functools
@@ -28,7 +32,7 @@ class DeepSpeech2:
         for _ in range(3):
             layer = tf.keras.layers.Bidirectional(
                 tf.keras.layers.LSTM(128, return_sequences=True,
-                                     recurrent_dropout=0.2))(layer)
+                                     recurrent_dropout=0))(layer)
             layer = tf.keras.layers.BatchNormalization()(layer)
 
         return layer
@@ -56,8 +60,11 @@ class DeepSpeech2RowConv:
 
         # RNN layers
         for _ in range(3):
-            layer = tf.keras.layers.LSTM(self.rnn_unit, return_sequences=True,
-                                         recurrent_dropout=0.2)(layer)
+            layer = tf.keras.layers.LSTM(self.rnn_unit, activation='tanh',
+                                         recurrent_activation='sigmoid',
+                                         return_sequences=True,
+                                         recurrent_dropout=0,
+                                         unroll=False, use_bias=True)(layer)
             layer = RowConv1D(filters=self.rnn_unit, future_context=2, strides=1,
                               padding="same")(layer)
             layer = tf.keras.layers.BatchNormalization()(layer)
