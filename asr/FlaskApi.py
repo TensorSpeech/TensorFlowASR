d768d218a779cd38e3e16d371d1edefe46f15a53
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2020-03-04 17:19:16 +0700

    Update dockerfile and flaskapi

--- asr/FlaskApi.py
+++ asr/FlaskApi.py
@@ -12,15 +12,30 @@ app.config.from_object(FlaskConfig)
 
 asr_blueprint = Blueprint("asr", __name__, url_prefix="/api")
 
+"""
+  Json request format:
+  {
+    "payload": "some data as bytes",
+  }
+"""
 
-@asr_blueprint.route("/static")
+
+@asr_blueprint.route("/static", methods=["POST"])
 def static_inference():
-  pass
+  """
+  Saves audio bytes from requests into a file, then transcribes that
+  file into text and return the text
+  :return: Json that contains the text
+  """
 
 
-@asr_blueprint.route("/streaming")
+@asr_blueprint.route("/streaming", methods=["POST"])
 def streaming_inference():
-  pass
+  """
+  Transcribes the audio bytes sent in realtime and
+  immediately returns the text at that time-step
+  :return: Json that contains the text
+  """
 
 
 app.register_blueprint(asr_blueprint)
