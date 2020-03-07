e45ec8c66a23d88f8b14364a7e6d07feb4cbe45c
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2020-03-07 13:39:14 +0700

    Update decoder and num classes

--- decoders/Decoders.py
+++ decoders/Decoders.py
@@ -54,7 +54,7 @@ class BeamSearchDecoder(Decoder):
                                           beam_width=self.beam_width)
     # decoded shape = [batch_size, top_path=1, decoded index]
     # get the first object of the list of top-path objects
-    decoded = decoded[0]
+    decoded = tf.squeeze(decoded[0], axis=1)
     return self.convert_to_string(decoded)
 
 
