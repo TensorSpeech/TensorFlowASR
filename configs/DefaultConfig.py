5942c14760f1435531770661ac6bd2698da2cdf6
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2020-04-08 22:38:18 +0700

    Update default config

--- configs/DefaultConfig.py
+++ configs/DefaultConfig.py
@@ -4,7 +4,7 @@ from models.deepspeech2.DeepSpeech2 import DeepSpeech2
 from augmentations.Augments import TimeWarping, TimeMasking, \
   FreqMasking
 
-base_model = DeepSpeech2()
+base_model = DeepSpeech2(num_conv=2, num_rnn=3, rnn_units=256)
 
 decoder = {
   "name": "beamsearch",
