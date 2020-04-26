264cf5567e238cc09bbd43f881cf57722345f6a7
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2020-04-26 13:23:36 +0700

    Update

--- models/CTCModel.py
+++ models/CTCModel.py
@@ -58,27 +58,25 @@ class CTCModel:
     model = tf.keras.Model(inputs=features, outputs=outputs)
     return model
 
+  # @tf.function
+  # def loss(self, y_true, y_pred, input_length, label_length):
+  #   loss = tf.keras.backend.ctc_batch_cost(
+  #     y_pred=y_pred,
+  #     input_length=tf.expand_dims(input_length, -1),
+  #     y_true=tf.squeeze(y_true, -1),
+  #     label_length=tf.expand_dims(label_length, -1))
+  #   return tf.reduce_mean(mask_nan(loss))
+
   @tf.function
   def loss(self, y_true, y_pred, input_length, label_length):
-    loss = tf.keras.backend.ctc_batch_cost(
-      y_pred=y_pred,
-      input_length=tf.expand_dims(input_length, -1),
-      y_true=tf.squeeze(y_true, -1),
-      label_length=tf.expand_dims(label_length, -1))
+    loss = tf.nn.ctc_loss(
+      labels=tf.cast(tf.squeeze(y_true, -1), tf.int32),
+      logit_length=input_length,
+      logits=y_pred,
+      label_length=label_length,
+      logits_time_major=False,
+      blank_index=self.num_classes - 1)
     return tf.reduce_mean(mask_nan(loss))
-
-  # @tf.function
-  # def loss(self, y_true, y_pred):
-  #   label_length = get_length(y_true)
-  #   input_length = get_length(y_pred)
-  #   loss = tf.nn.ctc_loss(
-  #     labels=tf.cast(tf.squeeze(y_true, -1), tf.int32),
-  #     logit_length=input_length,
-  #     logits=y_pred,
-  #     label_length=label_length,
-  #     logits_time_major=False,
-  #     blank_index=self.num_classes - 1)
-  #   return tf.reduce_mean(mask_nan(loss))
 
   def predict(self, *args, **kwargs):
     return self.model.predict(*args, **kwargs)
