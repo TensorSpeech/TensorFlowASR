70217d749d96897e78492b41fd5dfacb5403c6ab
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2020-04-27 17:02:56 +0700

    Add Noise Augmentation

--- augmentations/Augments.py
+++ augmentations/Augments.py
@@ -1,10 +1,11 @@
 from __future__ import absolute_import
 
 import functools
-import numpy as np
+import os
+import glob
 from augmentations.SpecAugment import time_warping, time_masking, \
   freq_masking
-from augmentations.NoiseAugment import add_white_noise, add_noise_from_sound
+from augmentations.NoiseAugment import add_noise
 
 
 def no_aug(features):
@@ -40,18 +41,15 @@ class TimeWarping(Augmentation):
     super().__init__(func=time_warping, is_post=True, **kwargs)
 
 
-class WhiteNoise(Augmentation):
-  def __init__(self, snr=10, **kwargs):
-    self.snr = snr
-    super().__init__(func=functools.partial(add_white_noise, snr=self.snr), is_post=False, **kwargs)
-
-
-class RealWorldNoise(Augmentation):
-  def __init__(self, noise_wavs: list, snr=10, **kwargs):
-    if not noise_wavs or len(noise_wavs) == 0:
-      raise ValueError("List of noise wav files must be defined")
-    self.noise_wavs = noise_wavs
-    self.snr = snr
-    super(RealWorldNoise, self).__init__(
-      func=functools.partial(add_noise_from_sound, noise_wavs=self.noise_wavs, snr=self.snr),
-      is_post=False, **kwargs)
+class Noise(Augmentation):
+  def __init__(self, min_snr: int, max_snr: int, min_noises: int, max_noises: int, noise_dir: str, **kwargs):
+    self.min_snr = min_snr
+    self.max_snr = max_snr
+    self.min_noises = min_noises
+    self.max_noises = max_noises
+    self.noises = glob.glob(os.path.join(noise_dir, "**", "*.wav"), recursive=True)
+    self.noises.append("white_noise")
+    super().__init__(func=functools.partial(
+      add_noise, min_snr=self.min_snr,
+      max_snr=self.max_snr, min_noises=self.min_noises,
+      max_noises=self.max_noises, noises=self.noises), is_post=False, **kwargs)
--- augmentations/NoiseAugment.py
+++ augmentations/NoiseAugment.py
@@ -6,7 +6,7 @@ import random
 from featurizers.SpeechFeaturizer import read_raw_audio
 
 
-def add_white_noise(signal: np.ndarray, snr=10, sample_rate=16000):
+def get_white_noise(signal: np.ndarray, snr=10):
   RMS_s = math.sqrt(np.mean(signal ** 2))
   # RMS values of noise
   RMS_n = math.sqrt(RMS_s ** 2 / (pow(10, snr / 20)))
@@ -16,18 +16,15 @@ def add_white_noise(signal: np.ndarray, snr=10, sample_rate=16000):
   # because mean=0 STD=RMS
   STD_n = RMS_n
   noise = np.random.normal(0, STD_n, signal.shape[0])
-  return signal + noise
+  return noise
 
 
-def add_noise_from_sound(signal: np.ndarray, noise_wavs: list, snr=10, sample_rate=16000):
-  noise = random.choice(noise_wavs)  # randomly choose a noise from a list of noises
-  noise = read_raw_audio(noise, sample_rate)
-
+def get_noise_from_sound(signal: np.ndarray, noise: np.ndarray, snr=10):
   if len(noise) < len(signal):
-    raise ValueError("Noise wav must be longer than speech")
-  else:
-    idx = random.choice(range(0, len(noise) - len(signal)))  # randomly crop noise wav
-    noise = noise[idx:idx + len(signal)]
+    return None
+
+  idx = random.choice(range(0, len(noise) - len(signal)))  # randomly crop noise wav
+  noise = noise[idx:idx + len(signal)]
 
   RMS_s = math.sqrt(np.mean(signal ** 2))
   # required RMS of noise
@@ -38,3 +35,21 @@ def add_noise_from_sound(signal: np.ndarray, noise_wavs: list, snr=10, sample_ra
   noise = noise * (RMS_n / RMS_n_current)
 
   return noise
+
+
+def add_noise(signal: np.ndarray, noises: list, min_snr: int, max_snr: int,
+              min_noises: int, max_noises: int, sample_rate=16000):
+  random.shuffle(noises)
+  num_noises = random.randint(min_noises, max_noises)
+  selected_noises = random.choices(noises, k=num_noises)
+  added_noises = []
+  for noise_type in selected_noises:
+    snr = random.uniform(min_snr, max_snr)
+    if noise_type == "white_noise":
+      added_noises.append(get_white_noise(signal, snr))
+    else:
+      noise = read_raw_audio(noise_type, sample_rate=sample_rate)
+      added_noises.append(get_noise_from_sound(signal, noise, snr))
+  for noise in added_noises:
+    signal = np.add(signal, noise)
+  return signal
