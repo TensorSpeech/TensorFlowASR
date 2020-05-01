39a3b9d48fe05d235bdf37e8596b3ce8978f74c0
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2020-05-01 21:19:59 +0700

    Update

--- asr/SpeechToText.py
+++ asr/SpeechToText.py
@@ -117,7 +117,7 @@ class SpeechToText:
         train_loss = train_step(feature, transcript, label_length)
         epoch_train_loss.append(train_loss)
 
-        sys.stdout.write("\033[K")
+        # sys.stdout.write("\033[K")
         tf.print(f"Epoch: {epoch + 1}/{epochs}, batch: {batch_idx}/{num_batch}, train_loss = {train_loss}", end="\r")
         batch_idx += 1
 
