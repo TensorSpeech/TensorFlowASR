8f112b29853feec113a8543f42dc5e8c41a06a18
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2020-05-23 09:19:02 +0700

    Update script

--- run_stt.py
+++ run_stt.py
@@ -106,13 +106,13 @@ def main():
         eval_data = config["eval_data_transcript_paths"]
         augmentations = config["augmentations"]
 
-        train_dataset = Dataset(config["train_data_transcript_paths"], tfrecords_dir, mode="train")
-        train_dataset.create_tfrecords(augmentations=augmentations)
-        test_dataset = Dataset(config["test_data_transcript_paths"], tfrecords_dir, mode="test")
-        test_dataset.create_tfrecords(augmentations=[None])
+        train_dataset = Dataset(config["train_data_transcript_paths"], mode="train")
+        train_dataset.create_tfrecords(tfrecords_dir, True)
+        test_dataset = Dataset(config["test_data_transcript_paths"], mode="test")
+        test_dataset.create_tfrecords(tfrecords_dir, False)
         if eval_data:
-            eval_dataset = Dataset(eval_data, tfrecords_dir, mode="eval")
-            eval_dataset.create_tfrecords(augmentations=[None])
+            eval_dataset = Dataset(eval_data, mode="eval")
+            eval_dataset.create_tfrecords(tfrecords_dir, False)
 
     elif args.mode == "convert_to_tflite":
         assert args.export_file and args.output_file_path
