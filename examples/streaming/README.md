038e35413481bf8002590f0772e1a199d543ae3e
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2020-09-30 22:57:57 +0700

    :rocket: Add streaming example

---
+++ examples/streaming/README.md
@@ -0,0 +1,5 @@
+# Python Audio Streaming for TFLITE
+
+## Dependencies
+
+- [python-sounddevice](https://python-sounddevice.readthedocs.io/en/0.4.1/installation.html)
---
+++ examples/streaming/audio_conformer.py
@@ -0,0 +1,118 @@
+# Copyright 2020 Huy Le Nguyen (@usimarit)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+import queue
+import sys
+import threading
+
+import sounddevice as sd
+import soundfile as sf
+
+
+def int_or_str(text):
+    """Helper function for argument parsing."""
+    try:
+        return int(text)
+    except ValueError:
+        return text
+
+
+parser = argparse.ArgumentParser(add_help=False)
+
+parser.add_argument("-l", "--list-devices", action="store_true",
+                    help="show list of audio devices and exit")
+
+args, remaining = parser.parse_known_args()
+
+if args.list_devices:
+    print(sd.query_devices())
+    parser.exit(0)
+
+parser = argparse.ArgumentParser(
+    description=__doc__,
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+    parents=[parser]
+)
+
+parser.add_argument("filename", metavar="FILENAME",
+                    help="audio file to be played back")
+
+parser.add_argument("-d", "--device", type=int_or_str,
+                    help="output device (numeric ID or substring)")
+
+parser.add_argument("-b", "--blocksize", type=int, default=2048,
+                    help="block size (default: %(default)s)")
+
+parser.add_argument("-q", "--buffersize", type=int, default=20,
+                    help="number of blocks used for buffering (default: %(default)s)")
+
+args = parser.parse_args(remaining)
+
+if args.blocksize == 0:
+    parser.error("blocksize must not be zero")
+if args.buffersize < 1:
+    parser.error("buffersize must be at least 1")
+
+q = queue.Queue(maxsize=args.buffersize)
+event = threading.Event()
+
+
+def callback(outdata, frames, time, status):
+    assert frames == args.blocksize
+    if status.output_underflow:
+        print("Output underflow: increase blocksize?", file=sys.stderr)
+        raise sd.CallbackAbort
+    assert not status
+    try:
+        data = q.get_nowait()
+    except queue.Empty as e:
+        print("Buffer is empty: increase buffersize?", file=sys.stderr)
+        raise sd.CallbackAbort from e
+    if len(data) < len(outdata):
+        outdata[:len(data)] = data
+        outdata[len(data):] = b"\x00" * (len(outdata) - len(data))
+        raise sd.CallbackStop
+    else:
+        outdata[:] = data
+
+
+try:
+    with sf.SoundFile(args.filename) as f:
+        for _ in range(args.buffersize):
+            data = f.buffer_read(args.blocksize, dtype="float32")
+            if not data:
+                break
+            q.put_nowait(data)  # Pre-fill queue
+        stream = sd.RawOutputStream(
+            samplerate=f.samplerate, blocksize=args.blocksize,
+            device=args.device, channels=f.channels, dtype="float32",
+            callback=callback, finished_callback=event.set
+        )
+        with stream:
+            timeout = args.blocksize * args.buffersize / f.samplerate
+            while data:
+                data = f.buffer_read(args.blocksize, dtype="float32")
+                q.put(data, timeout=timeout)
+            event.wait()  # Wait until playback is finished
+
+except KeyboardInterrupt:
+    parser.exit("\nInterrupted by user")
+
+except queue.Full:
+    # A timeout occurred, i.e. there was an error in the callback
+    parser.exit(1)
+
+except Exception as e:
+    parser.exit(type(e).__name__ + ": " + str(e))
