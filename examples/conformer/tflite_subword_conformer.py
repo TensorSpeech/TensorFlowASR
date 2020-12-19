5226a6a1ce5924d51dcff8e1b0229b6513da70e0
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2020-12-20 01:20:42 +0700

    :rocket: Fix tflite conversion

--- examples/conformer/tflite_subword_conformer.py
+++ examples/conformer/tflite_subword_conformer.py
@@ -64,9 +64,9 @@ conformer.add_featurizers(speech_featurizer, text_featurizer)
 
 concrete_func = conformer.make_tflite_function(greedy=True).get_concrete_function()
 converter = tf.lite.TFLiteConverter.from_concrete_functions([concrete_func])
+converter.experimental_new_converter = True
 converter.optimizations = [tf.lite.Optimize.DEFAULT]
-converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS,
-                                       tf.lite.OpsSet.SELECT_TF_OPS]
+converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS, tf.lite.OpsSet.SELECT_TF_OPS]
 tflite_model = converter.convert()
 
 if not os.path.exists(os.path.dirname(args.output)):
