536bf46c38f96a3dc35547d6dde4c284453bfa8a
M. Yusuf Sarıgöz <yusufsarigoz@gmail.com>
2021-01-03 15:07:08 +0300

    Add support for TPU training

--- tensorflow_asr/utils/__init__.py
+++ tensorflow_asr/utils/__init__.py
@@ -61,11 +61,11 @@ def setup_strategy(devices):
     return tf.distribute.MirroredStrategy()
 
 
-# def setup_tpu(tpu_address):
+# def setup_tpu(tpu_address=None):
 #     import tensorflow as tf
 
-#     resolver = tf.distribute.cluster_resolver.TPUClusterResolver(tpu='grpc://' + tpu_address)
-#     tf.config.experimental_connect_to_cluster(resolver)
-#     tf.tpu.experimental.initialize_tpu_system(resolver)
-#     print("All TPUs: ", tf.config.list_logical_devices('TPU'))
-#     return resolver
+    resolver = tf.distribute.cluster_resolver.TPUClusterResolver() if tpu_address is None else tf.distribute.cluster_resolver.TPUClusterResolver(tpu='grpc://' + tpu_address)
+    tf.config.experimental_connect_to_cluster(resolver)
+    tf.tpu.experimental.initialize_tpu_system(resolver)
+    print("All TPUs: ", tf.config.list_logical_devices('TPU'))
+    return tf.distribute.TPUStrategy(resolver)
