6cb3a4990c94f8178425485f1390b673e74a918d
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2021-02-07 23:09:07 +0700

    :writing_hand: fix some minor bugs

--- tensorflow_asr/datasets/asr_dataset.py
+++ tensorflow_asr/datasets/asr_dataset.py
@@ -140,7 +140,7 @@ class ASRDataset(BaseDataset):
 
         # PREFETCH to improve speed of input length
         dataset = dataset.prefetch(AUTOTUNE)
-        self.total_steps = get_num_batches(self.total_steps, batch_size)
+        self.total_steps = get_num_batches(self.total_steps, batch_size, drop_remainders=self.drop_remainder)
         return dataset
 
     @tf.function
