ac216b2a4c46e6ed20e3593c33a54340a2a19fd2
Andres Elizondo <andres.e@adestech.mx>
2021-04-16 16:17:43 -0500

    Adds ASRMaskedSliceDataset, for generating rolling masks for Streaming Conformer.

--- tensorflow_asr/datasets/asr_dataset.py
+++ tensorflow_asr/datasets/asr_dataset.py
@@ -332,3 +332,115 @@ class ASRSliceDataset(ASRDataset):
         dataset = tf.data.Dataset.from_tensor_slices(self.entries)
         dataset = dataset.map(self.load, num_parallel_calls=AUTOTUNE)
         return self.process(dataset, batch_size)
+
+
+class ASRMaskedSliceDataset(ASRSliceDataset):
+    """ Dataset for ASR with rolling mask """
+
+    def __init__(self,
+                 stage: str,
+                 speech_featurizer: SpeechFeaturizer,
+                 text_featurizer: TextFeaturizer,
+                 data_paths: list,
+                 augmentations: Augmentation = Augmentation(None),
+                 cache: bool = False,
+                 shuffle: bool = False,
+                 indefinite: bool = False,
+                 drop_remainder: bool = True,
+                 use_tf: bool = False,
+                 buffer_size: int = BUFFER_SIZE,
+                 history_window_size: int = 3,
+                 input_chunk_duration: int = 250,
+                 **kwargs):
+        super(ASRMaskedSliceDataset, self).__init__(
+            data_paths=data_paths, augmentations=augmentations,
+            cache=cache, shuffle=shuffle, stage=stage, buffer_size=buffer_size,
+            drop_remainder=drop_remainder, use_tf=use_tf, indefinite=indefinite,
+            speech_featurizer=speech_featurizer, text_featurizer=text_featurizer
+        )
+        self.speech_featurizer = speech_featurizer
+        self.text_featurizer = text_featurizer
+        self.history_window_size = history_window_size
+        self.input_chunk_size = input_chunk_duration * self.speech_featurizer.sample_rate // 1000
+
+    def calculate_mask(self, num_frames):
+        frame_step = self.speech_featurizer.frame_step
+        frames_per_chunk = self.input_chunk_size // frame_step
+
+        time_reduction_factor = 4 # TODO: Get time_reduction_factor from config or model.
+        num_frames = tf.cast(tf.math.ceil(num_frames / time_reduction_factor), tf.int32)
+
+        def _calculate_mask(num_frames, frames_per_chunk, history_window_size):
+            mask = np.zeros((num_frames, num_frames), dtype=np.int32)
+            for i in range(num_frames):
+                # Frames in the same chunk can see each other
+                # If frames in `history_window_size` are in other chunks, the full chunks are visible
+                current_chunk_index = i // frames_per_chunk
+                history_chunk_index = (i - history_window_size) // frames_per_chunk
+                for curr in range(history_chunk_index, current_chunk_index + 1):
+                    for j in range(frames_per_chunk):
+                        base_index = curr * frames_per_chunk
+                        if base_index + j < 0 or base_index + j >= num_frames:
+                            continue
+                        mask[i, base_index + j] = 1
+            return mask
+
+        return tf.numpy_function(
+            _calculate_mask, inp=[num_frames, frames_per_chunk, self.history_window_size], Tout=tf.int32
+        )
+
+    def preprocess(self, path: tf.Tensor, audio: tf.Tensor, indices: tf.Tensor):
+        preprocessed_inputs = super(ASRMaskedSliceDataset, self).preprocess(path, audio, indices)
+
+        input_length = preprocessed_inputs[2]
+        mask = self.calculate_mask(input_length)
+        mask.set_shape((None, None))
+
+        return (*preprocessed_inputs, mask)
+
+    def tf_preprocess(self, path: tf.Tensor, audio: tf.Tensor, indices: tf.Tensor):
+        preprocessed_inputs = super(ASRMaskedSliceDataset, self).tf_preprocess(path, audio, indices)
+
+        input_length = preprocessed_inputs[2]
+        mask = self.calculate_mask(input_length)
+        mask.set_shape((None, None))
+
+        return (*preprocessed_inputs, mask)
+
+    # -------------------------------- CREATION -------------------------------------
+
+    def process(self, dataset: tf.data.Dataset, batch_size: int):
+        dataset = dataset.map(self.parse, num_parallel_calls=AUTOTUNE)
+
+        if self.cache:
+            dataset = dataset.cache()
+
+        if self.shuffle:
+            dataset = dataset.shuffle(self.buffer_size, reshuffle_each_iteration=True)
+
+        if self.indefinite:
+            dataset = dataset.repeat()
+
+        # PADDED BATCH the dataset
+        dataset = dataset.padded_batch(
+            batch_size=batch_size,
+            padded_shapes=(
+                tf.TensorShape([]),
+                tf.TensorShape(self.speech_featurizer.shape),
+                tf.TensorShape([]),
+                tf.TensorShape(self.text_featurizer.shape),
+                tf.TensorShape([]),
+                tf.TensorShape(self.text_featurizer.prepand_shape),
+                tf.TensorShape([]),
+                tf.TensorShape([self.speech_featurizer.shape[0], self.speech_featurizer.shape[0]])
+            ),
+            padding_values=(None, 0., 0, self.text_featurizer.blank, 0, self.text_featurizer.blank, 0, 0),
+            drop_remainder=self.drop_remainder
+        )
+
+        # PREFETCH to improve speed of input length
+        dataset = dataset.prefetch(AUTOTUNE)
+        self.total_steps = get_num_batches(self.total_steps, batch_size, drop_remainders=self.drop_remainder)
+        return dataset
+
+# TODO: Create masked TFRecords dataset
