360ddf31025bbe1af7f8276905e782856e68df52
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2021-04-24 15:43:14 +0700

    :writing_hand: update speech featurizer

--- tensorflow_asr/featurizers/speech_featurizers.py
+++ tensorflow_asr/featurizers/speech_featurizers.py
@@ -95,7 +95,7 @@ def normalize_audio_feature(audio_feature: np.ndarray, per_frame=False):
     """ Mean and variance normalization """
     axis = 1 if per_frame else None
     mean = np.mean(audio_feature, axis=axis)
-    std_dev = np.std(np.variance(audio_feature, axis=axis) + 1e-9)
+    std_dev = np.std(audio_feature, axis=axis) + 1e-9
     normalized = (audio_feature - mean) / std_dev
     return normalized
 
@@ -111,10 +111,8 @@ def tf_normalize_audio_features(audio_feature: tf.Tensor, per_frame=False):
     """
     axis = 1 if per_frame else None
     mean = tf.reduce_mean(audio_feature, axis=axis, keepdims=True)
-    std_dev = tf.math.sqrt(tf.math.reduce_variance(audio_feature, axis=axis, keepdims=True) + 1e-9)
+    std_dev = tf.math.reduce_std(audio_feature, axis=axis, keepdims=True) + 1e-9
     return (audio_feature - mean) / std_dev
-    # audio_feature, _ = tf.linalg.normalize(audio_feature, axis=axis)
-    # return audio_feature
 
 
 def normalize_signal(signal: np.ndarray):
