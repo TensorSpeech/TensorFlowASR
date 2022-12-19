d21c5a2026e56f524e68d717ad60983d8a2e3584
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2022-12-19 09:37:01 +0700

    fix: update naive rnnt loss and grad

--- tests/test_rnnt_loss.py
+++ tests/test_rnnt_loss.py
@@ -2,18 +2,29 @@ import tensorflow as tf
 
 from tensorflow_asr.losses.rnnt_loss_naive import compute_rnnt_loss_and_grad_helper
 
+B = 1
+T = 20
+U = 10
+V = 1000
+blank = 0
 
-def test():
-    tf.config.run_functions_eagerly(True)
-    logits = tf.random.normal([1, 20, 11, 10], dtype=tf.float32)
-    labels = tf.range(10, dtype=tf.int32)[None, :]
-    logit_length = tf.convert_to_tensor([20], dtype=tf.int32)
-    label_length = tf.convert_to_tensor([10], dtype=tf.int32)
+
+@tf.function
+def run():
+    logits = tf.random.normal([B, T, U + 1, V], dtype=tf.float32)
+    labels = tf.range(U, dtype=tf.int32)[None, :]
+    logit_length = tf.convert_to_tensor([T], dtype=tf.int32)
+    label_length = tf.convert_to_tensor([U], dtype=tf.int32)
 
     compute_rnnt_loss_and_grad_helper(
         logits=logits,
         labels=labels,
         label_length=label_length,
         logit_length=logit_length,
-        blank=0,
+        blank=blank,
     )
+
+
+def test():
+    tf.config.run_functions_eagerly(False)
+    run()
