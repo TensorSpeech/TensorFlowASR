d69c4ace28964e3cbc2bcd58a7aa24431d55a0d9
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2023-02-12 19:10:37 +0700

    fix: wp featurizer vocab size

--- tests/test_wp_featurizer.py
+++ tests/test_wp_featurizer.py
@@ -14,7 +14,7 @@ decoder_config = DecoderConfig(
     }
 )
 
-text = "i'm good but it would have broken down after ten miles of that hard trail dawn came while they wound over the crest of the range and with the sun in their faces they took the downgrade it was well into the morning before nash reached logan"
+text = "<pad> i'm good but it would have broken down after ten miles of that hard trail dawn came while they wound over the crest of the range and with the sun in their faces they took the downgrade it was well into the morning before nash reached logan"
 
 
 def test_wordpiece_featurizer():
