fix: update attention mask and rnnt loss logits length

tensorflow_asr/losses/rnnt_loss.py
@@ -52,23 +52,26 @@ class RnntLoss(tf.keras.losses.Loss):
             logger.info("Use GPU/TPU implementation for RNNT loss")
 
     def call(self, y_true, y_pred):
+        # logits, logit_length, labels, label_length = y_pred["logits"], y_pred["logits_length"], y_true["labels"], y_true["labels_length"]
+        logits, labels, label_length = y_pred["logits"], y_true["labels"], y_true["labels_length"]
+        logit_length = tf.repeat(tf.shape(logits)[1], repeats=tf.shape(logits)[0])
         if self.use_cpu:
             with tf.device("/CPU:0"):
                 return rnnt_loss(
-                    logits=y_pred["logits"],
-                    logit_length=y_pred["logits_length"],
-                    labels=y_true["labels"],
-                    label_length=y_true["labels_length"],
+                    logits=logits,
+                    logit_length=logit_length,
+                    labels=labels,
+                    label_length=label_length,
                     blank=self.blank,
                     name=self.name,
                     use_cpu=self.use_cpu,
                 )
         else:
             return rnnt_loss(
-                logits=y_pred["logits"],
-                logit_length=y_pred["logits_length"],
-                labels=y_true["labels"],
-                label_length=y_true["labels_length"],
+                logits=logits,
+                logit_length=logit_length,
+                labels=labels,
+                label_length=label_length,
                 blank=self.blank,
                 name=self.name,
                 use_cpu=self.use_cpu,

tensorflow_asr/models/layers/multihead_attention.py
@@ -13,8 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import math
-
 import tensorflow as tf
 from keras.layers import EinsumDense
 from keras.layers import MultiHeadAttention as KerasMultiHeadAttention
@@ -116,9 +114,9 @@ class MultiHeadAttention(KerasMultiHeadAttention):
             mask_expansion_axis = -len(self._attention_axes) * 2 - 1
             for _ in range(len(attention_scores.shape) - len(attention_mask.shape)):
                 attention_mask = tf.expand_dims(attention_mask, axis=mask_expansion_axis)
-            smallest_negative = tf.convert_to_tensor(math_util.large_compatible_negative(attention_scores.dtype), dtype=attention_scores.dtype)
-            attention_scores += (1.0 - tf.cast(attention_mask, attention_scores.dtype)) * smallest_negative
-            attention_scores = tf.where(tf.math.is_inf(attention_scores), smallest_negative, attention_scores)
+            attention_scores = math_util.masked_fill(
+                attention_scores, mask=attention_mask, value=math_util.large_compatible_negative(attention_scores.dtype)
+            )
         attention_scores = self._softmax(attention_scores)
         return attention_scores
 

tensorflow_asr/models/layers/subsampling.py
@@ -22,11 +22,13 @@ class Subsampling(Layer):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.time_reduction_factor = 1
+        self._do_apply_mask = True
 
     def call(self, inputs):
         outputs, inputs_length = inputs  # outputs already reduced, inputs_length is original
         outputs_length = math_util.get_reduced_length(inputs_length, self.time_reduction_factor)
-        outputs = math_util.apply_mask(outputs, mask=tf.sequence_mask(outputs_length, maxlen=tf.shape(outputs)[1], dtype=tf.bool))
+        if self._do_apply_mask:
+            outputs = math_util.apply_mask(outputs, mask=tf.sequence_mask(outputs_length, maxlen=tf.shape(outputs)[1], dtype=tf.bool))
         return outputs, outputs_length
 
     def compute_mask(self, inputs, mask=None):
@@ -120,6 +122,7 @@ class VggSubsampling(Subsampling):
         )
         self.maxpool2 = tf.keras.layers.MaxPool2D(pool_size=pool_size, strides=strides, padding=padding, name="maxpool_2")
         self.time_reduction_factor = self.maxpool1.pool_size[0] * self.maxpool2.pool_size[0]
+        self._do_apply_mask = False
 
     def call(self, inputs, training=False):
         outputs, inputs_length = inputs
@@ -186,6 +189,7 @@ class Conv2dSubsampling(Subsampling):
             subblock.add(tf.keras.layers.Activation(activation, name=f"{activation}_{i}"))
             self.convs.append(subblock)
             self.time_reduction_factor *= strides
+        self._do_apply_mask = False
 
     def call(self, inputs, training=False):
         outputs, inputs_length = inputs
@@ -234,6 +238,7 @@ class Conv1dSubsampling(Subsampling):
             subblock.add(tf.keras.layers.Activation(activation, name=f"{activation}_{i}"))
             self.convs.append(subblock)
             self.time_reduction_factor *= strides
+        self._do_apply_mask = False
 
     def call(self, inputs, training=False):
         outputs, inputs_length = inputs

tensorflow_asr/utils/math_util.py
@@ -188,10 +188,10 @@ def masked_fill(
     mask,
     value=0,
 ):
-    shape = shape_util.shape_list(tensor)
-    mask = tf.broadcast_to(mask, shape)
-    values = tf.cast(tf.fill(shape, value), tensor.dtype)
-    return tf.where(mask, tensor, values)
+    _mask = tf.cast(mask, tensor.dtype)
+    tensor *= _mask
+    tensor += (1.0 - _mask) * tf.cast(tf.convert_to_tensor(value, tensor.dtype), tensor.dtype)
+    return tensor
 
 
 def large_compatible_negative(
@@ -202,7 +202,10 @@ def large_compatible_negative(
     return -1e9
 
 
-def apply_mask(outputs, mask=None):
+def apply_mask(
+    outputs,
+    mask=None,
+):
     if mask is not None:
         expanded_mask = mask
         for _ in range(len(outputs.shape) - len(mask.shape)):  # expand last axis of mask so that it's dim equals output's dim