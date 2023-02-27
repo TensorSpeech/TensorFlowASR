5324f2155da62cf13eef9112d1dea19f29cfba92
Nguyễn Lê Huy <nlhuy.cs.16@gmail.com>
2023-02-27 17:00:33 +0700

    fix: relpe

--- tensorflow_asr/models/layers/positional_encoding.py
+++ tensorflow_asr/models/layers/positional_encoding.py
@@ -23,7 +23,7 @@ def compute_sinusoid_position_encoding(
     dtype=tf.float32,
 ):
     # length of sequence is the second last dimension of the inputs
-    position = tf.cast(tf.range(0, max_length, 1), dtype)
+    position = tf.cast(tf.range(max_length - 1, -1, -1), dtype)
     min_freq = tf.cast(1 / 10000.0, dtype=dtype)
     timescales = tf.pow(min_freq, tf.cast(2 * (tf.range(dmodel) // 2), dtype) / tf.cast(dmodel, dtype))
     angles = tf.expand_dims(position, 1) * tf.expand_dims(timescales, 0)
