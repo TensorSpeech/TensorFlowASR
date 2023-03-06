83d6680b48a7942d6691b828ae709331aa2c4787
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2023-03-06 10:35:19 +0700

    fix: update relpe

--- tests/test_relpe.py
+++ tests/test_relpe.py
@@ -6,7 +6,7 @@ from tensorflow_asr.utils import math_util, plot_util
 
 def test():
     batch_size, input_length, max_length, dmodel = 1, 450, 500, 144
-    pe = positional_encoding.compute_sinusoid_position_encoding(input_length, max_length, dmodel)
+    pe = positional_encoding.compute_sinusoid_position_encoding(max_length, dmodel)
     pe = pe.numpy().T
     print(pe.shape)
     plot_util.plotmesh(pe, title="sinusoid position encoding")
