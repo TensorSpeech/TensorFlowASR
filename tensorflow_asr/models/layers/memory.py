81a323f8d81bfab3cbc0216a97e9152b00864f5f
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2023-05-03 02:23:50 +0700

    fix: update

--- tensorflow_asr/models/layers/memory.py
+++ tensorflow_asr/models/layers/memory.py
@@ -18,6 +18,27 @@ from tensorflow_asr.models.base_layer import Layer
 from tensorflow_asr.utils import math_util
 
 
+def _prepend_memory_item(
+    per_batch_memory,  # [M, D]
+    per_batch_memory_mask,  # [M]
+    per_batch_input,  # [T, D]
+    per_batch_input_mask,  # [T]
+    pad_right=True,
+):
+    memory_mask_count = math_util.count(tf.cast(per_batch_memory_mask, tf.int32), value=0)
+    input_mask_count = math_util.count(tf.cast(per_batch_input_mask, tf.int32), value=0)
+
+    # [M + T, D]
+    per_batch_new_inputs = tf.concat([tf.stop_gradient(tf.roll(per_batch_memory, shift=memory_mask_count, axis=0)), per_batch_input], 0)
+    per_batch_new_inputs_mask = tf.concat([tf.roll(per_batch_memory_mask, shift=memory_mask_count, axis=0), per_batch_input_mask], 0)
+
+    if not pad_right:
+        per_batch_new_inputs = tf.roll(per_batch_new_inputs, shift=input_mask_count, axis=0)
+        per_batch_new_inputs_mask = tf.roll(per_batch_new_inputs_mask, shift=input_mask_count, axis=0)
+
+    return per_batch_memory, per_batch_memory_mask, per_batch_new_inputs, per_batch_new_inputs_mask
+
+
 class Memory(Layer):
     def __init__(self, batch_size, memory_length, dmodel, **kwargs):
         super().__init__(**kwargs)
@@ -41,34 +62,13 @@ class Memory(Layer):
             name="memory_mask",
         )
 
-    def _prepend_memory_item(
-        self,
-        per_batch_memory,  # [M, D]
-        per_batch_memory_mask,  # [M]
-        per_batch_input,  # [T, D]
-        per_batch_input_mask,  # [T]
-        pad_right=True,
-    ):
-        memory_mask_count = math_util.count(tf.cast(per_batch_memory_mask, tf.int32), value=0)
-        input_mask_count = math_util.count(tf.cast(per_batch_input_mask, tf.int32), value=0)
-
-        # [M + T, D]
-        per_batch_new_inputs = tf.concat([tf.stop_gradient(tf.roll(per_batch_memory, shift=memory_mask_count, axis=0)), per_batch_input], 0)
-        per_batch_new_inputs_mask = tf.concat([tf.roll(per_batch_memory_mask, shift=memory_mask_count, axis=0), per_batch_input_mask], 0)
-
-        if not pad_right:
-            per_batch_new_inputs = tf.roll(per_batch_new_inputs, shift=input_mask_count, axis=0)
-            per_batch_new_inputs_mask = tf.roll(per_batch_new_inputs_mask, shift=input_mask_count, axis=0)
-
-        return per_batch_memory, per_batch_memory_mask, per_batch_new_inputs, per_batch_new_inputs_mask
-
     def attach_memory(self, inputs):
         inputs_mask = getattr(inputs, "_keras_mask", None)
         max_length = tf.shape(inputs)[1]
         if inputs_mask is None:
             inputs_mask = tf.ones([self.batch_size, max_length], dtype=tf.bool)
         _, _, new_inputs, new_inputs_mask = tf.vectorized_map(
-            lambda item: self._prepend_memory_item(*item), elems=(tf.stop_gradient(self.memory), self.memory_mask, inputs, inputs_mask), warn=False
+            lambda item: _prepend_memory_item(*item), elems=(tf.stop_gradient(self.memory), self.memory_mask, inputs, inputs_mask), warn=False
         )
         new_inputs._keras_mask = new_inputs_mask  # pylint: disable=protected-access
         return new_inputs
@@ -78,7 +78,7 @@ class Memory(Layer):
         if inputs_mask is None:
             inputs_mask = tf.ones([self.batch_size, tf.shape(inputs)[1]], dtype=tf.bool)
         _, _, new_memory, new_memory_mask = tf.vectorized_map(
-            lambda item: self._prepend_memory_item(*item, pad_right=False), elems=(self.memory, self.memory_mask, inputs, inputs_mask), warn=False
+            lambda item: _prepend_memory_item(*item, pad_right=False), elems=(self.memory, self.memory_mask, inputs, inputs_mask), warn=False
         )
         new_memory = tf.slice(
             new_memory,
