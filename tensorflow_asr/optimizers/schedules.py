3f3933af06fc1aac8c7946c8bb99c260804e130d
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2023-05-28 19:24:21 +0700

    fix: add initial lr in transformer scheduler

--- tensorflow_asr/optimizers/schedules.py
+++ tensorflow_asr/optimizers/schedules.py
@@ -16,20 +16,22 @@ import tensorflow as tf
 
 
 class TransformerSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
-    def __init__(self, d_model, warmup_steps=4000, max_lr=None, min_lr=None):
+    def __init__(self, d_model, initial_lr=1.0, warmup_steps=4000, max_lr=None, min_lr=None):
         super().__init__()
-        self.d_model = d_model
-        self.d_model = tf.cast(self.d_model, tf.float32)
+        self.d_model = tf.convert_to_tensor(d_model, dtype=tf.float32)
+        self.initial_lr = tf.convert_to_tensor(initial_lr, dtype=tf.float32)
+        self.warmup_steps = tf.convert_to_tensor(warmup_steps, dtype=tf.float32)
         self.max_lr = max_lr
         self.min_lr = min_lr
-        self.warmup_steps = warmup_steps
 
     def __call__(self, step):
         # lr = (d_model^-0.5) * min(step^-0.5, step*(warm_up^-1.5))
-        step = tf.cast(step, dtype=tf.float32)
-        arg1 = tf.math.rsqrt(step)
-        arg2 = step * (self.warmup_steps**-1.5)
-        lr = tf.math.rsqrt(self.d_model) * tf.math.minimum(arg1, arg2)
+        step = tf.convert_to_tensor(step, dtype=tf.float32)
+        if self.warmup_steps > 0:
+            lr = (self.d_model**-0.5) * tf.math.minimum(step**-0.5, step * (self.warmup_steps**-1.5))
+        else:
+            lr = (self.d_model**-0.5) * (step**-0.5)
+        lr = self.initial_lr * lr
         if self.max_lr is not None:
             lr = tf.math.minimum(self.max_lr, lr)
         if self.min_lr is not None:
@@ -42,27 +44,6 @@ class TransformerSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
             "warmup_steps": self.warmup_steps,
             "max_lr": self.max_lr,
             "min_lr": self.min_lr,
-        }
-
-
-class SANSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
-    def __init__(self, lamb, d_model, warmup_steps=4000):
-        super().__init__()
-        self.lamb = tf.cast(lamb, tf.float32)
-        self.d_model = tf.cast(d_model, tf.float32)
-        self.warmup_steps = tf.cast(warmup_steps, tf.float32)
-
-    def __call__(self, step):
-        step = tf.cast(step, dtype=tf.float32)
-        arg1 = step / (self.warmup_steps**1.5)
-        arg2 = 1 / tf.math.sqrt(step)
-        return (self.lamb / tf.math.sqrt(self.d_model)) * tf.math.minimum(arg1, arg2)
-
-    def get_config(self):
-        return {
-            "lamb": self.lamb,
-            "d_model": self.d_model,
-            "warmup_steps": self.warmup_steps,
         }
 
 
