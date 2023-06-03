36fda64651ea9ace4b95fdfdc1bcb6711c11ba73
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2023-06-03 16:48:01 +0700

    fix: general example scripts

--- examples/train.py
+++ examples/train.py
@@ -1,4 +1,4 @@
-# Copyright 2020 Huy Le Nguyen (@nglehuy)
+# Copyright 2023 Huy Le Nguyen (@nglehuy)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,13 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import tensorflow as tf
-
+from tensorflow_asr import tf  # import to aid logging messages
 from tensorflow_asr.configs.config import Config
 from tensorflow_asr.helpers import dataset_helpers, featurizer_helpers
 from tensorflow_asr.utils import cli_util, env_util, file_util
-
-logger = tf.get_logger()
 
 
 def main(
--- tensorflow_asr/__init__.py
+++ tensorflow_asr/__init__.py
@@ -1,10 +1,10 @@
 import os
 import warnings
 
-import tensorflow as tf
-
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = os.environ.get("TF_CPP_MIN_LOG_LEVEL", "2")
 os.environ["TF_FORCE_GPU_ALLOW_GROWTH"] = os.environ.get("TF_FORCE_GPU_ALLOW_GROWTH", "true")
+
+import tensorflow as tf
 
 logger = tf.get_logger()
 logger.setLevel(os.environ.get("LOG_LEVEL", "info").upper())
