fd6b6c2adc1f7a43463fd515d28caed2df032b48
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2023-06-11 10:54:56 +0700

    chore: test

--- tests/test_wp_featurizer.py
+++ tests/test_wp_featurizer.py
@@ -5,16 +5,18 @@ import tensorflow as tf
 
 from tensorflow_asr.configs.config import DecoderConfig
 from tensorflow_asr.featurizers.text_featurizers import WordPieceFeaturizer
+from tensorflow_asr.utils import file_util
 
-decoder_config = DecoderConfig(
-    {
-        "vocabulary": f"{os.path.dirname(__file__)}/../vocabularies/librispeech/wordpiece/train_1000_50.tokens",
-        "max_subword_length": 50,
-        "unknown_token": "<unk>",
-    }
-)
+file_util.ENABLE_PATH_PREPROCESS = False
+
+config_path = os.path.join(file_util.ROOT_DIRECTORY, "examples", "configs", "wp_whitespace.yml.j2")
+print(config_path)
+config = file_util.load_yaml(config_path)
+
+decoder_config = DecoderConfig(config["decoder_config"])
 
 text = "<pad> i'm good but it would have broken down after ten miles of that hard trail dawn came while they wound over the crest of the range and with the sun in their faces they took the downgrade it was well into the morning before nash reached logan"
+text = "a b"
 
 
 def test_wordpiece_featurizer():
@@ -22,8 +24,6 @@ def test_wordpiece_featurizer():
     print(featurizer.num_classes)
     print(text)
     indices = featurizer.extract(text)
-    print(indices.numpy())
-    indices = featurizer.tf_extract(text)
     print(indices.numpy())
     batch_indices = tf.stack([indices, indices], axis=0)
     reversed_text = featurizer.iextract(batch_indices)
