a80cc506998464c7838052f1e0c5fbbdb69c0d31
nglehuy <nlhuy.cs.16@gmail.com>
2023-07-26 13:08:44 +0700

    fix: ds2

--- tensorflow_asr/models/ctc/deepspeech2.py
+++ tensorflow_asr/models/ctc/deepspeech2.py
@@ -189,7 +189,7 @@ class RnnBlock(Layer):
         return outputs, outputs_length
 
     def compute_output_shape(self, input_shape):
-        return input_shape
+        return input_shape[:-1] + [self.rnn.units]
 
 
 class RnnModule(Layer):
@@ -227,7 +227,7 @@ class RnnModule(Layer):
         return outputs
 
     def compute_output_shape(self, input_shape):
-        return input_shape
+        return input_shape[:-1] + [self.blocks[-1].rnn.units]
 
 
 # ------------------------------ FULLY CONNECTED ----------------------------- #
@@ -255,7 +255,7 @@ class FcBlock(Layer):
         return outputs, outputs_length
 
     def compute_output_shape(self, input_shape):
-        return input_shape
+        return input_shape[:-1] + [self.fc.units]
 
 
 class FcModule(Layer):
@@ -276,7 +276,7 @@ class FcModule(Layer):
         return outputs
 
     def compute_output_shape(self, input_shape):
-        return input_shape
+        return input_shape[:-1] + [self.blocks[-1].fc.units]
 
 
 class DeepSpeech2Encoder(Layer):
