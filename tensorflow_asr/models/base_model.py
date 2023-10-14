45d4d0e93afec5a4add17ca5f8115272b64ac624
nglehuy <nlhuy.cs.16@gmail.com>
2023-10-14 20:56:37 +0700

    fix: accumulate

--- tensorflow_asr/models/base_model.py
+++ tensorflow_asr/models/base_model.py
@@ -220,14 +220,14 @@ class BaseModel(tf.keras.Model):
             gradients, caching = self._train_step(data, caching=caching)
         else:
             if caching is None:
-                for i in range(self.ga.total_steps):
+                for i in tf.range(self.ga.total_steps):
                     per_ga_step_data = tf.nest.map_structure(
                         lambda x: math_util.slice_batch_tensor(x, index=i, batch_size=self._per_replica_batch_size), data
                     )
                     per_ga_gradients, _ = self._train_step(per_ga_step_data)
                     self.ga.accumulate(per_ga_gradients)
             else:
-                for i in range(self.ga.total_steps):
+                for i in tf.range(self.ga.total_steps):
                     per_ga_step_data = tf.nest.map_structure(
                         lambda x: math_util.slice_batch_tensor(x, index=i, batch_size=self._per_replica_batch_size), data
                     )
@@ -262,7 +262,9 @@ class BaseModel(tf.keras.Model):
             self._test_step(data)
         else:
             for i in tf.range(self.ga.total_steps):
-                per_ga_step_data = tf.nest.map_structure(lambda x: x[i * self._per_replica_batch_size : (i + 1) * self._per_replica_batch_size], data)
+                per_ga_step_data = tf.nest.map_structure(
+                    lambda x: math_util.slice_batch_tensor(x, index=i, batch_size=self._per_replica_batch_size), data
+                )
                 self._test_step(per_ga_step_data)
         metrics = self.get_metrics_result()
         metrics = tf.nest.map_structure(lambda x: x / self.distribute_strategy.num_replicas_in_sync, metrics)
--- tensorflow_asr/optimizers/accumulation.py
+++ tensorflow_asr/optimizers/accumulation.py
@@ -23,39 +23,19 @@ class GradientAccumulator:
         if model.trainable_variables is None:
             raise ValueError("trainable_variables must be defined")
         self._ga_steps = ga_steps
-        self._accum_step = tf.Variable(
-            tf.constant(0, dtype=tf.int32),
-            trainable=False,
-            synchronization=tf.VariableSynchronization.ON_READ,
-            aggregation=tf.VariableAggregation.ONLY_FIRST_REPLICA,
-            name="accum_step",
-        )
         self._gradients = [None for _ in model.trainable_variables]
         self._gradient_inited = False
-
-    @property
-    def step(self):
-        """Number of accumulated steps."""
-        return self._accum_step.value()
 
     @property
     def total_steps(self):
         return self._ga_steps
 
     @property
-    def is_apply_step(self):
-        return tf.equal(self.step, self.total_steps)
-
-    @property
     def gradients(self):
         """The accumulated gradients on the current replica."""
         if not self._gradient_inited:
             raise ValueError("gradients are not initialized")
-        return tf.cond(  # zeros gradients so that apply_gradient has no effect
-            self.is_apply_step,
-            lambda: [gradient.value() if gradient is not None else gradient for gradient in self._gradients],
-            lambda: [tf.zeros_like(gradient) if gradient is not None else gradient for gradient in self._gradients],
-        )
+        return list(gradient.value() if gradient is not None else gradient for gradient in self._gradients)
 
     def accumulate(self, gradients):
         """Accumulates :obj:`gradients` on the current replica."""
@@ -74,11 +54,9 @@ class GradientAccumulator:
         for accum_gradient, gradient in zip(self._gradients, gradients):
             if gradient is not None and accum_gradient is not None:
                 accum_gradient.assign_add(gradient, read_value=False)
-        self._accum_step.assign_add(1)
 
     def reset(self):
         """Resets the accumulated gradients on the current replica."""
-        self._accum_step.assign(0)
         for gradient in self._gradients:
             if gradient is not None:
                 gradient.assign(tf.zeros_like(gradient), read_value=False)
