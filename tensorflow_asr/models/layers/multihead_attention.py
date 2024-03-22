fix: softmax

tensorflow_asr/models/layers/multihead_attention.py
@@ -20,7 +20,6 @@ import tensorflow as tf
 from keras.layers import EinsumDense
 from keras.layers import MultiHeadAttention as KerasMultiHeadAttention
 
-from tensorflow_asr.models.activations.softmax import Softmax
 from tensorflow_asr.models.layers.memory import Memory
 from tensorflow_asr.utils import shape_util
 from tensorflow_asr.utils.env_util import KERAS_SRC
@@ -245,7 +244,7 @@ class MultiHeadAttention(KerasMultiHeadAttention):
             attn_scores_rank,
         ) = mha_module._build_attention_equation(rank, attn_axes=self._attention_axes)
         norm_axes = tuple(range(attn_scores_rank - len(self._attention_axes), attn_scores_rank))
-        self._softmax = Softmax(axis=norm_axes, dtype=self.dtype)  # stable training
+        self._softmax = tf.keras.layers.Softmax(axis=norm_axes, dtype=self.dtype)  # stable training
         self._dropout_layer = tf.keras.layers.Dropout(rate=self._dropout, dtype=self.dtype)
 
     def _masked_softmax(self, attention_scores, attention_mask=None):