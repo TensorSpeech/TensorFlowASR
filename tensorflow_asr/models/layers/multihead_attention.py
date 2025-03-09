fix: streaming masking mha

tensorflow_asr/models/layers/multihead_attention.py
@@ -181,8 +181,8 @@ def compute_attention_mask(
         causal mask if `use_causal_mask=True`.
     """
     query_mask = getattr(query, "_keras_mask", None)
-    value_mask = getattr(value, "_keras_mask", None)
-    key_mask = getattr(key, "_keras_mask", None)
+    value_mask = None  # getattr(value, "_keras_mask", None)
+    key_mask = None  # getattr(key, "_keras_mask", None)
     auto_mask = None
     if query_mask is not None:
         query_mask = tf.cast(query_mask, tf.bool)  # defensive casting