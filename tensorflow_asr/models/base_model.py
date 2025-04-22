fix: update gradient accumulation

tensorflow_asr/models/base_model.py
@@ -112,7 +112,8 @@ class BaseModel(keras.Model, TensorFlowTrainer):
         optimizer = keras.optimizers.get(optimizer)
         if isinstance(ga_steps, int) and ga_steps > 1:
             self.use_ga = True
-            self.ga = GradientAccumulator(ga_steps=ga_steps)
+            self.ga = GradientAccumulator(ga_steps=ga_steps, optimizer=optimizer)
+            self.ga.build(self.trainable_weights)
             kwargs["steps_per_execution"] = 1
             logger.info(f"Using gradient accumulation with accumulate steps = {ga_steps}")
         else:
@@ -187,13 +188,14 @@ class BaseModel(keras.Model, TensorFlowTrainer):
         metrics = self.get_metrics_result()
         return metrics
 
-    def train_step_ga(self, data_buffer):  # avoid merge_call error as "Such behaviors are not yet supported"
-        first_data, *rest_data = data_buffer
-        gradients = self._train_step(first_data)
-        for data in rest_data:
-            next_gradients = self._train_step(data)
-            gradients = self.ga.accumulate(gradients, next_gradients)
-        self._apply_gradients(gradients)
+    def train_step_ga(self, data, do_apply=None):  # avoid merge_call error as "Such behaviors are not yet supported"
+        gradients = self._train_step(data)
+        if do_apply is None:
+            self.ga.accumulate(gradients, self.trainable_weights)
+        else:
+            gradients = self.ga.gradients(gradients, self.trainable_weights)
+            self._apply_gradients(gradients)
+            self.ga.reset()
         metrics = self.get_metrics_result()
         return metrics
 
@@ -273,8 +275,8 @@ class BaseModel(keras.Model, TensorFlowTrainer):
             return self.train_function
 
         @tf.autograph.experimental.do_not_convert
-        def one_ga_step_on_data(data_buffer):
-            outputs = self.distribute_strategy.run(self.train_step_ga, args=(data_buffer,))
+        def one_ga_step_on_data(data, do_apply=None):
+            outputs = self.distribute_strategy.run(self.train_step_ga, args=(data, do_apply))
             outputs = reduce_per_replica(
                 outputs,
                 self.distribute_strategy,
@@ -290,10 +292,12 @@ class BaseModel(keras.Model, TensorFlowTrainer):
             )
 
         def function(iterator):
-            data_buffer = []
-            for _, data in zip(range(self.ga.total_steps), iterator):
-                data_buffer.append(data)
-            return one_ga_step_on_data(data_buffer)
+            for step, data in zip(range(self.ga.total_steps), iterator):
+                if step >= self.ga.total_steps - 1:
+                    outputs = one_ga_step_on_data(data, True)
+                else:
+                    outputs = one_ga_step_on_data(data)
+            return outputs
 
         self.train_function = function
         return self.train_function

tensorflow_asr/optimizers/accumulation.py
@@ -2,6 +2,8 @@
 Gradient Accummulation for training TF2 custom training loop.
 """
 
+from keras.src.optimizers.base_optimizer import BaseOptimizer
+
 from tensorflow_asr import tf
 
 
@@ -10,20 +12,59 @@ class GradientAccumulator:
     # performed on assignment. To get the value, we call .value() which returns the
     # value on the current replica without synchronization.
 
-    def __init__(self, ga_steps, name="ga"):
+    def __init__(self, ga_steps, optimizer: BaseOptimizer, name="ga"):
         self.name = name
         if ga_steps is None:
             raise ValueError("ga_steps must be defined")
         self._ga_steps = ga_steps
+        self._optimizer = optimizer
+        self._accumulated_gradients = []
+        self.built = False
+
+    def build(self, variables):
+        if not self._optimizer.built:
+            self._optimizer.build(variables)
+        for i, variable in enumerate(variables):
+            self._accumulated_gradients.append(
+                self._optimizer.add_variable_from_reference(
+                    variable,
+                    name="gradient_accumulator",
+                )
+            )
+        self.built = True
 
     @property
     def total_steps(self):
         return self._ga_steps
 
-    def is_apply_step(self, step):
-        return tf.math.equal(step % self._ga_steps, 0)
+    # def is_apply_step(self, step):
+    #     return tf.math.equal(step % self._ga_steps, 0)
 
-    def accumulate(self, gradients, per_ga_gradients):
+    def reset(self):
+        for g_acc in self._accumulated_gradients:
+            g_acc.assign(tf.zeros(g_acc.shape, dtype=g_acc.dtype))
+
+    def _get_acc_grads(self, trainable_variables):
+        # `trainable_variables` might have been filtered in previous
+        # processing steps, so we need to ensure the correct mapping between
+        # `self._accumulated_gradients` and `trainable_variables`
+        acc_grads = [self._accumulated_gradients[self._optimizer._get_variable_index(v)] for v in trainable_variables]
+        return acc_grads
+
+    def accumulate(self, grads, trainable_variables):
         """Accumulates :obj:`gradients` on the current replica."""
-        with tf.name_scope(self.name):
-            return [None if x is None else x if y is None else x + y for x, y in zip(gradients, per_ga_gradients)]
+        if not self.built:
+            self.build(trainable_variables)
+        # return [None if x is None else x if y is None else x + y for x, y in zip(gradients, per_ga_gradients)]
+        acc_grads = self._get_acc_grads(trainable_variables)
+        new_g_accs = [(g + acc_g) for g, acc_g in zip(grads, acc_grads)]
+        for n_g_acc, g_acc in zip(new_g_accs, acc_grads):
+            g_acc.assign(n_g_acc)
+
+    def gradients(self, grads, trainable_variables):
+        """Gets the gradients for the apply step."""
+        if not self.built:
+            self.build(trainable_variables)
+        acc_grads = self._get_acc_grads(trainable_variables)
+        grads = [(g + acc_g) / self._ga_steps for g, acc_g in zip(grads, acc_grads)]
+        return grads